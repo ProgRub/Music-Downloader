2c81b8c2647ad94926cf5ff3789a71f2cf264fe7
Ruben Rodrigues <ruben2000@gmail.com>
2021-06-29 18:05:04 +0100

    DTO

--- Music-Downloader/Business/DTOs/MusicFileDTO.cs
+++ Music-Downloader/Business/DTOs/MusicFileDTO.cs
@@ -1,10 +1,12 @@
-﻿namespace Business.DTOs
+﻿using System.Collections.Generic;
+
+namespace Business.DTOs
 {
     public class MusicFileDTO
     {
         public string Filename { get; set; }
         public string AlbumArtist { get; set; }
-        public string ContributingArtists { get; set; }
+        public IEnumerable<string> ContributingArtists { get; set; }
         public string Album { get; set; }
         public string SongTitle { get; set; }
         public string Genre { get; set; }
